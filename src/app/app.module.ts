4d123bc0557739eea55385b450ebb75e59fd5491
Manuel <ManuelDiazGil96@hotmail.com>
2020-03-24 11:51:35 +0100

    Primer componente trip-detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TripDisplayComponent } from './components/trip/trip-display/trip-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TripDisplayComponent
   ],
   imports: [
     BrowserModule
