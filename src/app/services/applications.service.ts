81b37591465c55bc2e2c994d1ea153a8ffa6832c
jossanjim3 <jossanjim3@gmail.com>
2020-04-15 20:22:54 +0200

    añade fecha creacion a datateble applications

--- src/app/services/applications.service.ts
+++ src/app/services/applications.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Application } from '../models/application.model';
+import { AuthService } from './auth.service';
+import { Actor } from '../models/actor.model';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +12,31 @@ import { Application } from '../models/application.model';
 export class ApplicationsService {
 
   constructor(
-    private http: HttpClient) {
+    private http: HttpClient, private authService: AuthService, private messageService: MessageService) {
   }
 
   getApplications() {
-    // const url = `${environment.backendApiBaseURL}/v1/applications/users/${userId}`;
-    const url = `${environment.backendApiBaseURL}/v1/applications`;
+    let url = '';
+     url = `${environment.backendApiBaseURL}/v1/applications`;
+
+    /* this.authService.getCurrentActor()
+      .then( (actorData: Actor) => {
+        // console.log('actorData subscribe ngOnInit: ' + actorData);
+
+        if (actorData !== null) {
+          // console.log('actorData ngOnInit: ' + actorData);
+          const userId = actorData._id;
+          console.log('userId: ' + userId);
+          url = `${environment.backendApiBaseURL}/v1/applications/users/${userId}`;
+          console.log('url: ' + url);
+        }
+
+      })
+    .catch( (err) => {
+      console.log(err);
+      this.messageService.notifyMessage(err, 'alert alert-success');
+    }); */
+
     return this.http.get<Application[]>(url).toPromise();
   }
 
