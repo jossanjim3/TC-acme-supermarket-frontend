d305d6a33db72ddd2f04aafd00e9e8f0454f973e
jossanjim3 <jossanjim3@gmail.com>
2020-05-06 18:19:22 +0200

    editar audits funciona

--- src/app/models/audit.model.ts
+++ src/app/models/audit.model.ts
@@ -2,21 +2,11 @@ import { Entity } from './entity.model';
 
 export class Audit extends Entity {
     _id: string;
-    title:  {
-        type: String,
-    };
-    description: {
-        type: String,
-    };
-    atachment:  {
-        type: String,
-    };
-    auditor: {
-        type: String,
-    };
-    trip: {
-        type: String,
-    };
+    title: string;
+    description: string;
+    atachment: string;
+    auditor: string;
+    trip: string;
 
     constructor() {
         super();
